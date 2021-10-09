51bbec218241174515a94de18815ad61cc30fde8
YOUSEF ALSARRAJ <67869671+CaptainJow@users.noreply.github.com>
2021-10-09 20:49:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const daysEL = document.getElementById("day") ; 
+const hoursEL = document.getElementById("hour") ; 
+const minutesEL = document.getElementById("minute") ; 
+const secondsEL = document.getElementById("second") ; 
+
+const wanted_date = "1 jan 2022"; 
+
+function countdown(){
+    const currentDate = new Date() ; 
+   const  new_wanted_date = new Date(wanted_date) ; 
+    const seconds = (new_wanted_date - currentDate)/1000 ; 
+    const days = Math.floor( seconds/86400 ) ; 
+    const hours = Math.floor(seconds/3600) %24 ; 
+    const minutes = Math.floor(seconds/60) %60 ; 
+    const secondsss = Math.floor(seconds)%60 ; 
+
+    daysEL.innerHTML = days ; 
+    hoursEL.innerHTML = hours  ; 
+    minutesEL.innerHTML = minutes ; 
+    secondsEL.innerHTML = secondsss ; 
+
+}
+
+function punk(time){
+    return time <10 ?("0${time}") : time ; 
+}
+countdown() ; 
+
